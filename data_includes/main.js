960479aa39f9dbd0207f3fc60bf32ccbb00a436d
Adam Royer <ajroyer@ucla.edu>
2019-10-09 13:15:51 -0700

    uncommented newHtml command

--- data_includes/main.js
+++ data_includes/main.js
@@ -9,9 +9,9 @@ PennController( "consent",
     ,
     newText("<p> Below is a consent form you will need to read. After reading, please press the 'I consent to participating' button. If you do not consent, please close the page</p>")
     ,
-//    newHtml("consent", "consent.html")
-//        .print()
-//    ,
+    newHtml("consent", "consent.html")
+        .print()
+    ,
     newButton("I consent to participating")
         .print()
         .wait()
