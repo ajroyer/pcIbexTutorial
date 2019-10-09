314be58792c88d6e0b8e67f4740cbadac7c37946
Adam Royer <ajroyer@ucla.edu>
2019-10-09 13:08:16 -0700

    add consent for real this time

--- data_includes/main.js
+++ data_includes/main.js
@@ -1,6 +1,22 @@
 PennController.ResetPrefix(null)
 PennController.Sequence( "consent" , "final" )
 
+PennController( "consent",
+    defaultText
+        .print()
+    ,
+    newText("<h2>Consenting Process</h2>")
+    ,
+    newText("<p> Below is a consent form you will need to read. After reading, please press the 'I consent to participating' button. If you do not consent, please close the page</p>")
+    ,
+//    newHtml("consent", "consent.html")
+//        .print()
+//    ,
+    newButton("I consent to participating")
+        .print()
+        .wait()
+)
+.log( "uniqueid" , PennController.GetURLParameter( "id" ) )
 
 PennController( "final" ,
     newText("<p>Thank you for your participation!</p>")
