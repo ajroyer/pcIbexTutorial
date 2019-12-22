aba24b69a68a60d7cc8d5f6457ed3cfaacc8b728
Adam <adamroyer1337@gmail.com>
2019-12-21 20:46:55 -0800

    changed size of consent text

--- data_includes/main.js
+++ data_includes/main.js
@@ -52,7 +52,7 @@ PennController( "welcome",
 PennController( "consent",
     defaultText
         .print()
-        .settings.css("font-size", "50px")
+        .settings.css("font-size", "3em")
     ,
     newText("<h2>Consenting Process</h2>")
     ,
